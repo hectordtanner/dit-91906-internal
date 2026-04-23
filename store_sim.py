bb9af94cd6451452fd340551a3220073092514fe
hectorT <hectordtanner@gmail.com>
2026-04-23 13:13:58 +1200

    added nav bar functionality

--- store_sim.py
+++ store_sim.py
@@ -26,9 +26,9 @@ class StoreGUI:
         self.nav_bar.grid(row=0, column=0)
         self.sell_restock_frame.grid(row=1, column=0)
 
-        self.to_stock_button = tk.Button(self.nav_bar, text="Stocks")
-        self.to_restock_button = tk.Button(self.nav_bar, text="Sell & Restock")
-        self.to_create_button = tk.Button(self.nav_bar, text="Create product")
+        self.to_stock_button = tk.Button(self.nav_bar, text="Stocks", command=self.move_to_stock)
+        self.to_restock_button = tk.Button(self.nav_bar, text="Sell & Restock", command=self.move_to_sell_restock)
+        self.to_create_button = tk.Button(self.nav_bar, text="Create product", command=self.move_to_create)
         self.to_stock_button.grid(row=0, column=0)
         self.to_restock_button.grid(row=0, column=1)
         self.to_create_button.grid(row=0, column=2)
@@ -37,32 +37,45 @@ class StoreGUI:
         self.product_name_entry = tk.Entry(self.create_product_frame)
         self.base_price_label = tk.Label(self.create_product_frame, text="Base Price:")
         self.base_price_entry = tk.Entry(self.create_product_frame)
-        self.product_name_label.grid(row=1, column=0)
-        self.product_name_entry.grid(row=1, column=1)
-        self.base_price_label.grid(row=2, column=0)
-        self.base_price_entry.grid(row=2, column=1)
+        self.product_name_label.grid(row=0, column=0)
+        self.product_name_entry.grid(row=0, column=1)
+        self.base_price_label.grid(row=1, column=0)
+        self.base_price_entry.grid(row=1, column=1)
 
         self.confirm_product_button = tk.Button(self.create_product_frame, text="Confirm")
-        self.confirm_product_button.grid(row=3, column=0, columnspan=2)
-
-        self.stock_to_restock_button = tk.Button(self.stock_level_frame, text="Sell & Restock")
-        
-
-        self.restock_to_stock_button = tk.Button(self.sell_restock_frame, text="Stocks")
-        self.restock_to_create_button = tk.Button(self.sell_restock_frame, text="Create Product")
-        self.restock_to_stock_button.grid(row=0, column=0, columnspan=2)
-        self.restock_to_create_button.grid(row=0, column=2, columnspan=2)
+        self.confirm_product_button.grid(row=2, column=0, columnspan=2)
         
         self.sell_radiobutton = tk.Radiobutton(self.sell_restock_frame, text="Sell ", variable=self.sell_or_restock, value=True)
         self.restock_radiobutton = tk.Radiobutton(self.sell_restock_frame, text="Restock ", variable=self.sell_or_restock, value=False)
         self.sell_num_entry = tk.Entry(self.sell_restock_frame)
         self.of_label = tk.Label(self.sell_restock_frame, text=" of ")
         self.sell_of_dropdown = tk.OptionMenu(self.sell_restock_frame, self.selected_product, "--Choose an option--", *self.product_names)
-        self.sell_radiobutton.grid(row=1, column=0)
-        self.restock_radiobutton.grid(row=2, column=0)
-        self.sell_num_entry.grid(row=1, column=1, rowspan=2)
-        self.of_label.grid(row=1, column=2, rowspan=2)
-        self.sell_of_dropdown.grid(row=1, column=3, rowspan=2)
+        self.sell_radiobutton.grid(row=0, column=0)
+        self.restock_radiobutton.grid(row=1, column=0)
+        self.sell_num_entry.grid(row=0, column=1, rowspan=2)
+        self.of_label.grid(row=0, column=2, rowspan=2)
+        self.sell_of_dropdown.grid(row=0, column=3, rowspan=2)
+    
+    def reset_screen(self):
+        """Reset display"""
+        self.stock_level_frame.grid_forget()
+        self.sell_restock_frame.grid_forget()
+        self.create_product_frame.grid_forget()
+
+    def move_to_stock(self):
+        """Change to stock level screen"""
+        self.reset_screen()
+        self.stock_level_frame.grid(row=1, column=0)
+    
+    def move_to_sell_restock(self):
+        """Change to sell & restock level screen"""
+        self.reset_screen()
+        self.sell_restock_frame.grid(row=1, column=0)
+    
+    def move_to_create(self):
+        """Change to product creation screen"""
+        self.reset_screen()
+        self.create_product_frame.grid(row=1, column=0)
 
 
 if __name__ == "__main__":
